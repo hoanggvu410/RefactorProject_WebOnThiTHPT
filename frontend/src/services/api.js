4bd729e2e70219f278cc668d690e4305345d7fab
hoangvv <hoangvv@runsystem.net>
2026-06-15 16:04:09 +0700

    asdfg

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://refactorproject-webonthithpt.onrender.com/";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://refactorproject-webonthithpt.onrender.com";
 
 export function parseJwt(token) {
   try {
